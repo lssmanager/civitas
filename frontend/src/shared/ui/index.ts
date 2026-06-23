ed5b4383a0520fe1de8571598284bd936fec9fff
lssmanager <manager@learnsocialstudies.com>
2026-06-22 22:54:05 -0500

    export shared dashboard primitives

--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -1,12 +1,18 @@
 export { DataTable } from "./DataTable";
 export type { DataTableColumn } from "./DataTable";
+export { DashboardPanel } from "./DashboardPanel";
+export { EmptyMetricState } from "./EmptyMetricState";
 export { EmptyState } from "./EmptyState";
 export { ErrorState } from "./ErrorState";
+export { InstrumentationBadge } from "./InstrumentationBadge";
 export { KeyValueList } from "./KeyValueList";
+export { KpiGrid } from "./KpiGrid";
 export { LoadingState } from "./LoadingState";
 export { MetricCard } from "./MetricCard";
 export { PageCard } from "./PageCard";
 export { PageShell } from "./PageShell";
+export { StatusPill } from "./StatusPill";
+export { SystemCheckCard } from "./SystemCheckCard";
 
 export { JsonLogBlock } from "./JsonLogBlock";
 export { StatusBadge } from "./StatusBadge";
